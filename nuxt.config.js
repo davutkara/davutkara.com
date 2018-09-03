f9f8b678835264efb662aaf6d0da04e2e86171eb
Davut KARA <davutkara.comtr@yandex.com>
2018-09-03 23:10:34 +0300

    buefy eklendi

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,13 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Davutkara.com nuxtjs project' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Davutkara.com nuxtjs project'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
   ** Customize the progress bar color
@@ -24,16 +26,16 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
           exclude: /(node_modules)/
-        })
+        });
       }
     }
-  }
-}
-
+  },
+  modules: ['nuxt-buefy']
+};
