249b68e2c2af30897af4b00ca460b0d06e0647a0
Davut KARA <davutkara.comtr@yandex.com>
2018-09-05 23:52:23 +0300

    hero componenti yapıldı

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,20 @@
 <template>
-<section class="hero is-dark is-bold is-large">
-  <div class="hero-head">
-    <nav class="level">
-      <!-- Right side -->
-      <div class="level-right">
-        <p class="level-item"><strong>All</strong></p>
-        <p class="level-item"><a>Published</a></p>
-        <p class="level-item"><a>Drafts</a></p>
-        <p class="level-item"><a>Deleted</a></p>
-        <p class="level-item"><a class="button is-success">New</a></p>
-      </div>
-    </nav>
+  <div>
+    <hero title="Davut KARA" subTitle="İnformation Systems Engineer" />
   </div>
-
-  <div class="hero-body">
-    <div class="container has-text-centered">
-      <p class="title">
-        Davut KARA
-      </p>
-      <p class="subtitle">
-        Information Systems Engineer
-      </p>
-    </div>
-  </div>
-
-  <div class="hero-foot">
-    <nav class="tabs is-boxed is-fullwidth">
-      <div class="container">
-        <ul>
-          <li class="is-active">
-            <a>Yapım Aşamasında</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-</section>
 </template>
 
 <script>
+import hero from '~/components/hero.vue'
 export default {
   components: {
-
+    hero
   },
   mounted: function () {
     this.$toast.open({
       duration: 5000,
       message: `Site yapım aşamasındadır.`,
-      position: 'is-top',
+      position: 'is-bottom-right',
       type: 'is-info'
     })
   }
