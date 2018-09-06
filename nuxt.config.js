0d7c7d58509387af64d5088d5237b0e52b0b82d9
Davut KARA <davutkara.comtr@yandex.com>
2018-09-06 14:14:48 +0300

    başlık değiştirildi

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'davutkara.com',
+    title: 'Davut KARA',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
