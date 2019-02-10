0299aba94da71decfddbade54ec6f58ae3adfcea
Davut KARA <davutkara.comtr@yandex.com>
2019-02-10 15:07:25 +0300

    refactor: migrate v1 to v2

---
+++ components/footerCmp.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>All the content created by Davut KARA on this website can be used and shared with giving reference.</p>
+      <p>
+        Made with,
+        <strong>NuxtJS</strong>,
+        <strong>Buefy - Bulma</strong>,
+        served on
+        <strong>Netlify</strong>
+      </p>
+      <p>This website supports open source</p>
+      <a href="https://github.com/davutkara/davutkara.com" target="_blank">Source code</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'footerCmp'
+}
+</script>
---
+++ components/hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero is-dark is-bold is-medium">
+    <div class="hero-body">
+      <div class="container animate has-text-centered">
+        <p class="title">{{title}}</p>
+        <p class="subtitle">{{subTitle}}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['title', 'subTitle']
+}
+</script>
+
+<style>
+</style>
