d5948407c65557669a01462677a49f24c01816fb
Davut KARA <davutkara.comtr@yandex.com>
2019-02-13 00:47:40 +0300

    refactor: content as a prop instead of usigin slot

--- components/readMore.vue
+++ components/readMore.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <span v-show="!show" @click="show=true">
-      <a class="button is-small">
-        Show details
-      </a>
+      <a class="button is-small">Show details</a>
     </span>
-    <div v-show="show">
-      <slot />
-    </div>
+    <div v-show="show" v-html="content"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ReadMore',
+  props: {
+    content: {
+      type: String,
+      default: ''
+    }
+  },
   data: function() {
     return {
       show: false
