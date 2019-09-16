dd3a702eca3f73a6c9dca69657d41803696284da
Davut KARA <davutkara55@gmail.com>
2019-09-16 23:35:49 +0300

    refactor: use safety html

--- components/footer.vue
+++ components/footer.vue
@@ -2,16 +2,18 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p v-text="$t('copyright')" />
-      <p v-html="$t('how-to-made')" />
+      <p v-dompurify-html="$t('how-to-made')" />
       <p v-text="$t('open-source-message')" />
-      <a href="https://github.com/davutkara/davutkara.com" target="_blank">
-        {{ $t('source-codes') }}
-      </a>
+      <a href="https://github.com/davutkara/davutkara.com" target="_blank">{{ $t('source-codes') }}</a>
     </div>
   </footer>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueDOMPurifyHTML from 'vue-dompurify-html'
+Vue.use(VueDOMPurifyHTML, { default: { ALLOWED_TAGS: ['strong'] } })
+
 export default {
   name: 'FooterCmp'
 }
