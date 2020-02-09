9fcf3c10876055fd44899cf956c41345f6565ad2
Davut KARA <davutkara55@gmail.com>
2020-02-10 00:36:07 +0300

    feat: give link for quotes

--- components/cards/quote.vue
+++ components/cards/quote.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="card">
-    <div class="card-content">
-      <p class="title">
-        {{ title }}
-      </p>
-      <p class="subtitle">
-        {{ author }}
-      </p>
-      <time :datetime="date" class="is-pulled-right">{{ timeAgo(date) }}</time>
-      <div class="is-clearfix" />
+  <router-link :to="`${slug}`" :title="title">
+    <div class="card">
+      <div class="card-content">
+        <p class="title">{{ title }}</p>
+        <p class="subtitle">{{ author }}</p>
+        <time :datetime="date" class="is-pulled-right">{{ timeAgo(date) }}</time>
+        <div class="is-clearfix" />
+      </div>
+      <footer class="card-footer">
+        <p v-for="({name,url},index) in links" :key="index" class="card-footer-item">
+          <span>
+            <a :href="url" target="_blank">{{ name }}</a>
+          </span>
+        </p>
+      </footer>
     </div>
-    <footer class="card-footer">
-      <p v-for="({name,url},index) in links" :key="index" class="card-footer-item">
-        <span>
-          <a :href="url" target="_blank">{{ name }}</a>
-        </span>
-      </p>
-    </footer>
-  </div>
+  </router-link>
 </template>
 
 <script>
