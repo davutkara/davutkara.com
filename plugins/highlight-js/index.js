97449103313c60813c758c2d3442939eeb9fcd79
Davut KARA <davutkara.comtr@yandex.com>
2020-12-15 21:42:09 +0300

    fix: highlight.js 10x

--- plugins/highlight-js/index.js
+++ plugins/highlight-js/index.js
@@ -1,4 +1,4 @@
-import hljs from 'highlight.js/lib/highlight'
+import hljs from 'highlight.js/lib/core'
 import 'highlight.js/styles/github.css'
 import javascript from 'highlight.js/lib/languages/javascript'
 hljs.registerLanguage('javascript', javascript)
