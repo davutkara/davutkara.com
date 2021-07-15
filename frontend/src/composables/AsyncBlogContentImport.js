ac1d59a8f66109bf8916fdca69e5b496ff5e474a
Davut Kara <10008440+davutkara@users.noreply.github.com>
2021-07-15 17:11:30 +0300

    fix: no need to set

--- frontend/src/composables/AsyncBlogContentImport.js
+++ frontend/src/composables/AsyncBlogContentImport.js
@@ -53,12 +53,6 @@ export default function () {
       .then((file) => {
         content.value = file.default;
         isContentLoading.value = false;
-
-        if (content.value.alternate) {
-          route.meta.alternate = content.value.alternate
-        } else {
-          route.meta.alternate = null;
-        }
         return file.default;
       })
       .catch((err) => {
