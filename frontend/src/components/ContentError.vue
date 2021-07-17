b175144ec3a138a3365713c28e776edf6445d5e1
Davut Kara <10008440+davutkara@users.noreply.github.com>
2021-07-17 19:37:19 +0300

    fix: typo

--- frontend/src/components/ContentError.vue
+++ frontend/src/components/ContentError.vue
@@ -4,7 +4,7 @@
     <p>It is not a bug. It is a feature :)</p>
     <p>Just the page data could not be fetch.</p>
     <p>
-      It may be lost. It maybe does not exist or it may be connection error.
+      It may be lost. It maybe does not exist or it may be a connection error.
     </p>
   </article>
 </template>
