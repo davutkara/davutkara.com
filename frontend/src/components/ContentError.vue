26c3699df5b4304640e891ba9603bf7cd081e5d6
Davut Kara <10008440+davutkara@users.noreply.github.com>
2021-07-17 19:16:17 +0300

    feat: add a description

--- frontend/src/components/ContentError.vue
+++ frontend/src/components/ContentError.vue
@@ -1,6 +1,11 @@
 <template>
   <article>
     <h1>Error</h1>
+    <p>It is not a bug. It is a feature :)</p>
+    <p>Just the page data could not be fetch.</p>
+    <p>
+      It may be lost. It maybe does not exist or it may be connection error.
+    </p>
   </article>
 </template>
 
@@ -8,5 +13,4 @@
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>
