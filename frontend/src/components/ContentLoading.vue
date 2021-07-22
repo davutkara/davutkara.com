3604c54219fcb400064ea18ca60453c7e98b8043
Davut Kara <10008440+davutkara@users.noreply.github.com>
2021-07-22 19:09:14 +0300

    feat: add i18n messages

--- frontend/src/components/ContentLoading.vue
+++ frontend/src/components/ContentLoading.vue
@@ -1,14 +1,30 @@
 <template>
   <article>
-    <h1>Loading</h1>
+    <h1>{{ t("loading") }}</h1>
   </article>
 </template>
 
 <script>
+import { useI18n } from "vue-i18n";
+
 export default {
-}
+  setup() {
+    const { t } = useI18n({
+      useScope: "global",
+      locale: "en",
+      messages: {
+        en: {
+          loading: "Loading",
+        },
+        tr: {
+          loading: "Yükleniyor",
+        },
+      },
+    });
+
+    return { t };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
